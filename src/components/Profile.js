036fc6d5d01db7ca84f51b0eb06bc171da7bf3da
Gihan1937 <gihansen0216@gmial.com>
2022-01-24 16:07:13 -0500

    second commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,26 +3,46 @@ import './Profile.css';
 
 const Profile = () => {
 
+    const [users, setUsers] = useState([]);
+
+    const getUser = async () => {
+        try {
+            const response = await fetch("http://localhost:7200/users");
+            const userData = await response.json();
+
+            setUsers(userData);
+        } catch (err) {
+            console.error(err.message)
+        }
+    };
+
+    useEffect(() => {
+        getUser();
+    }, []);
 
 
     return (
-        <Fragment className="">
+        <Fragment>
             
-            <h1 className="p-5 bg-secondary text-center text_h1">  Profile  </h1>
-            <div class="text-center">
+            <h1 className=" container mt-5 text-center text_h1 ">  Profile  </h1>
+            <div className="text-center">
                 <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" className="image rounded-circle mt-5" alt="Profile Image">
                 </img>
             </div>
             
             <div className="d-flex flex-column mt-5 container  p-5">
                
-                   <h3 className = "text-center">Profile Info</h3> 
+                <h3 className = "text-center t_color">Profile Info</h3> 
             
-                <ul class="list-group list-group-flush">
-                     <li className="list-group-item">User Name:</li>
-                     <li className="list-group-item">Country:</li>
-                    <li className="list-group-item">Join Date:</li>
-                </ul>
+                
+                {users.map((u) => (
+                    <ul className="list-group list-group-flush" key={u.id}>
+                        <li className="list-group-item background">Name: {u.firstname} {u.lastname}</li>
+                        <li className="list-group-item background">UserName: {u.username}</li>
+                        <li className="list-group-item background">Country: {u.country} </li>
+                        <li className="list-group-item background">Join Date: {u.joindate}</li>
+                    </ul>
+                    ))}
             </div>
         
         </Fragment>
@@ -31,3 +51,4 @@ const Profile = () => {
 }
 
 export default Profile;
+
