65bb07c3d225e3b50481722a457519dd1b8beedc
Paul Okoye <Chibuzorokoye77@gmail.com>
2022-01-25 15:13:28 -0500

    Fixed Comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,7 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import axios from 'axios';
-
-
+import './Comment.css'
+import EditComment from './EditComments';
  
 const CommentList = () =>
 {
@@ -11,9 +11,9 @@ const CommentList = () =>
     {
         try {
             const url = "http://localhost:5000/comments"
-             axios.get(url).then(async ( response) => 
+             axios.get(url).then(async (response) => 
             {
-                const data = JSON.stringify(response.data)
+                const data = await response.data
                 setComList(data) 
                 console.log(comList) 
         })
@@ -25,6 +25,21 @@ const CommentList = () =>
     }
 
 
+    const deleteComments = async (id) => {
+
+
+        try {
+            const removeTodo = await fetch(`http://localhost:5000/comments/${id}`,
+            {
+                method: "DELETE",
+            })
+            setComList(comList.filter(comList =>comList.id !== id))
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+
     useEffect(() => {
         getComments()
     },[]) 
@@ -32,16 +47,25 @@ const CommentList = () =>
     return(
         <Fragment>
             <div>
-                    {/* {comList.map(comm =>(
-                        <div key={comm.id}>
-                            <p>{comm.description} {comm.stars}</p>
- 
-                        </div>
-                    ))} */}
-
-
-                    <p>Something should work</p>
+            <div className="container mt-3">
+    <div className="row d-flex mt-5 justify-content-center">
             </div>
+            {comList.map(comm =>(
+            <div className="card p-3 mt-3" key={comm.id}>
+            
+                <div className="d-flex justify-content-between align-items-center">
+                    <div className="user d-flex flex-row align-items-left"> <span> <small className="font-weight-bold">{comm.description} </small></span> </div>
+                
+                <div className="action d-flex justify-content-between mt-2 align-items-center">
+                    <div className="icons align-items-center"> <i className="fa fa-star text-warning"></i> <i className="fa fa-check-circle-o check-icon"></i> </div>
+                    <div> <small> 2 days ago</small> <small> <EditComment comList={comm}/></small> <button className="btn btn-danger"><small onClick={() => deleteComments(comm.id)}> Delete </small></button></div>
+                </div>
+                </div>
+                
+                </div>
+                ))}
+                </div>
+                </div>
         </Fragment>
     )
 }
--- src/components/EditComments.js
+++ src/components/EditComments.js
@@ -29,26 +29,25 @@ const EditComment = ({comList}) => {
 
 
     
-    console.log(comList)
     return(
         <Fragment>
-        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target= {`#id${todo.todo_id}`}>
+        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target= {`#id${comList.id}`}>
   Edit
 </button>
 
 {/* <!-- Modal --> */}
-<div className="modal fade" id = {`id${todo.todo_id }`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"onClick={() => setDescription (todo.description)}>
+<div className="modal fade" id = {`id${comList.id }`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"onClick={() => setDescription (comList.description)}>
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
-        <h5 className="modal-title">Edit Description</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => setDescription (todo.description)}></button>
+        <h5 className="modal-title">Edit Comment</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => setDescription (comList.description)}></button>
       </div>
       <div className="modal-body">
         <input type='text' className="form-control" value={description} onChange={e => setDescription(e.target.value)}/>
       </div>
       <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal"onClick={() => setDescription (todo.description)}>Close</button>
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal"onClick={() => setDescription (comList.description)}>Close</button>
         <button type="button" className="btn btn-primary" onClick={e => updateDescription(e)}>Save changes</button>
       </div>
     </div>
