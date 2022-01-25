04ce7f998ce7797ec96bd5c4984b54c3dc1075b8
jlorenzo58 <jlorenzo58@icloud.com>
2022-01-24 19:06:20 -0500

    deleted gamedetails

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TopGames from "./components/TopGames";
 import SearchField from "./components/SearchField";
 import NavBar from "./components/NavBar";
-import GameDetails from "./components/GameDetails";
 import './App.css';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path='/topgames' element={<TopGames />} />
           <Route path='/searchfield' element={<SearchField />} />
-          <Route path='/game/:name' element={<GameDetails />} />
         </Routes>
       </div>
     </Router>
@@ -22,5 +20,3 @@ function App() {
 }
 
 export default App;
-
-
