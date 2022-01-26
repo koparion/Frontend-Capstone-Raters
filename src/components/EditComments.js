f7249dfd245d86fa8cb95940be843f4ddb3a6da4
jlorenzo58 <jlorenzo58@icloud.com>
2022-01-26 10:30:45 -0500

    m

--- src/components/EditComments.js
+++ src/components/EditComments.js
@@ -6,39 +6,36 @@ import React,{Fragment, useState} from "react";
 
 const EditComment = ({comList}) => {
 
-    const [description, setDescription] = useState(comList.description)
+  const [description, setDescription] = useState(comList.description)
     
     
-    const updateDescription = async (e) =>{
+  const updateDescription = async (e) =>{
 
-        e.preventDefault()
-        try {
-            const body = {description}
-            const response = await fetch(`http://localhost:5000/comments/${comList.id}`,
-            {
-                method: "PUT",
-                headers:{"Content-Type":
-                "application/json"},
-                body: JSON.stringify(body)
-            })
-            window.location = "/"
-        } catch (err) {
-            console.error(err.message)
-        }
-
+    e.preventDefault()
+    try {
+      const body = {description}
+      const response = await fetch(`http://localhost:5000/comments/${comList.id}`,{
+        method: "PUT",
+        headers:{
+          "Content-Type":
+          "application/json"},
+          body: JSON.stringify(body)
+      })
+      window.location = "/"
+      }catch (err) {
+        console.error(err.message)
+      }
     } 
 
 
     
-    return(
-        <Fragment>
-        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target= {`#id${comList.id}`}>
-  Edit
-</button>
+  return(
+    <Fragment>
+    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target= {`#id${comList.id}`}>Edit</button>
 
-{/* <!-- Modal --> */}
-<div className="modal fade" id = {`id${comList.id}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"onClick={() => setDescription (comList.description)}>
-  <div className="modal-dialog">
+    {/* <!-- Modal --> */}
+    <div className="modal fade" id = {`id${comList.id}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"onClick={() => setDescription (comList.description)}>
+    <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
         <h5 className="modal-title">Edit Comment</h5>
